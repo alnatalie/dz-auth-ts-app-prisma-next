a0083b0a5473e3eab01782818d7bb6ff32964572
Natalie <natasha@email.com>
2025-02-20 20:18:10 +0700

    +устранение ошибок

--- app/custom/page.tsx
+++ app/custom/page.tsx
@@ -6,54 +6,54 @@ import { auth, signIn, signOut, } from "@/auth"
 export default function Page1(){
     return <>
         <h2>Custom Page</h2>
-        <Account/>
+        {/* <Account/> */}
     </>
 }
 
 
-export async function Account() {
-    const session = await auth();
+// export async function Account() {
+//     const session = await auth();
     
-    if (session?.user) {
-            return (
-                // const {user} = session;
-              <>
-                Signed in as {session.user?.name} ({session.user?.email}) <br />
-                {session.user?.image && <img src={session.user.image} style={{width:'100px',borderRadius:'50%'}}/>}<br />
-                <SignOut/>
-              </>
-            )
-          }
-          return <>
-          <SignIn/>
-          </>
+//     if (session?.user) {
+//             return (
+//                 // const {user} = session;
+//               <>
+//                 Signed in as {session.user?.name} ({session.user?.email}) <br />
+//                 {session.user?.image && <img src={session.user.image} style={{width:'100px',borderRadius:'50%'}}/>}<br />
+//                 <SignOut/>
+//               </>
+//             )
+//           }
+//           return <>
+//           <SignIn/>
+//           </>
                   
             
-}
+// }
 
-export function SignIn() {
-    return (
-      <>
-        <form
-          action={async () => {
-            "use server";
-            await signIn();
-          }}
-        >
-          <button type="submit">Sign in</button>
-        </form>
-      </>
-    );
-  }
+// export function SignIn() {
+//     return (
+//       <>
+//         <form
+//           action={async () => {
+//             "use server";
+//             await signIn();
+//           }}
+//         >
+//           <button type="submit">Sign in</button>
+//         </form>
+//       </>
+//     );
+//   }
 
-export function SignOut(){
-    return(
-        <form 
-        action={async()=>{
-            "use server"
-            await signOut()
-        }}>
-            <button>Sign out</button>
-        </form>
-    )
-}
+// export function SignOut(){
+//     return(
+//         <form 
+//         action={async()=>{
+//             "use server"
+//             await signOut()
+//         }}>
+//             <button>Sign out</button>
+//         </form>
+//     )
+// }
